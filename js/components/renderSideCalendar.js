eb47af11d5e3466266b8391da65e2735752ff6c0
ievast <ievast@wix.com>
2023-08-17 09:37:18 +0300

    Rendering side calendars days #1

--- js/components/renderSideCalendar.js
+++ js/components/renderSideCalendar.js
@@ -2,52 +2,44 @@ export function renderSideCalendar(month, year) {
   const sideCalendarListDOM = document.querySelector('.side-calendar__list');
   sideCalendarListDOM.innerHTML = '';
 
-  //month ---> 0-11
-  //   https://stackoverflow.com/questions/222309/calculate-last-day-of-month
-  const daysInMonth = new Date(year, month, 0).getDate();
-
   let day = 1;
 
-  //0-6 sunday-saturday
-  const firsWeekDayOfCrrMonth = new Date(year, month - 1, 1).getDay();
-  console.log({ firsWeekDayOfCrrMonth });
+  //   https://stackoverflow.com/questions/222309/calculate-last-day-of-month
+  const daysInCrrMonth = new Date(year, month, 0).getDate(); //month ---> 0-11
+  const firsWeekDayOfCrrMonth = new Date(year, month - 1, 1).getDay(); //0-6 sunday-saturday
 
-  const today = new Date();
-  const crrMonth = new Date(year, month - 1);
+  const daysInPrevMonth = new Date(year, month - 1, 0).getDate(); //month ---> 0-11
+  //   [{ day: 1, type: 'current_month'}]
+  function addLiDOM(innerText, classList) {
+    const sideCalendarDayDOM = document.createElement('li');
+    sideCalendarDayDOM.innerText = innerText;
+    sideCalendarDayDOM.classList.add(...classList);
+    sideCalendarListDOM.appendChild(sideCalendarDayDOM);
+  }
 
-  while (day <= daysInMonth) {
-    for (let weekday = 0; weekday < 7; weekday++) {
-      const crrWeekDay = new Date(year, month - 1, day).getDay();
+  //render days of before month
+  for (let x = firsWeekDayOfCrrMonth; x > 0; x--) {
+    const innerText = daysInPrevMonth - x + 1;
+    const classList = ['side-calendar__day', 'not-curr-month'];
 
-      if (day > daysInMonth) {
-        //   next month
-        const sideCalendarDayDOM = document.createElement('li');
-        sideCalendarDayDOM.innerText = day - daysInMonth;
-        sideCalendarDayDOM.classList.add('not-curr-month');
-        sideCalendarListDOM.appendChild(sideCalendarDayDOM);
-        day++;
-      }
+    addLiDOM(innerText, classList);
+  }
 
-      if (day <= daysInMonth) {
-        const sideCalendarDayDOM = document.createElement('li');
+  //render days of crr month
+  while (day <= daysInCrrMonth) {
+    const innerText = day++;
+    const classList = ['side-calendar__day'];
 
-        if (day === 1 && crrWeekDay !== weekday) {
-          //before month    REIKIA VAIDO
-          const daysInPrevMonth = new Date(year, month - 1, 0).getDate();
+    addLiDOM(innerText, classList);
+  }
 
-          console.log({ weekday, day });
+  //render days of next month
+  const daysInNextMonth = 42 - firsWeekDayOfCrrMonth - daysInCrrMonth;
 
-          sideCalendarDayDOM.innerText =
-            daysInPrevMonth - (firsWeekDayOfCrrMonth - weekday) + 1;
-          sideCalendarDayDOM.classList.add('not-curr-month');
-          sideCalendarListDOM.appendChild(sideCalendarDayDOM);
-          continue;
-        }
+  for (let i = 1; i <= daysInNextMonth; i++) {
+    const innerText = i;
+    const classList = ['side-calendar__day', 'not-curr-month'];
 
-        sideCalendarDayDOM.innerText = day++;
-        sideCalendarDayDOM.classList.add('side-calendar__day');
-        sideCalendarListDOM.appendChild(sideCalendarDayDOM);
-      }
-    }
+    addLiDOM(innerText, classList);
   }
 }
