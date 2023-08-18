92cc15940916004dce15576d21864f82f3636e4c
ievast <ievast@wix.com>
2023-08-18 15:02:40 +0300

    Added renderSideTitle to separate renderMainTitle

---
+++ js/components/renderSideTitle.js
@@ -0,0 +1,14 @@
+// import { renderSideCalendar } from './renderSideCalendar.js';
+
+export function doTitleDate(title, activeDayData) {
+  renderTitle(activeDayData, title);
+}
+
+function renderTitle(date, title) {
+  const formattedDateYear = date.toLocaleString('en-US', {
+    year: 'numeric'
+  });
+  const formattedDateMonth = date.toLocaleString('en-US', { month: 'long' });
+
+  title.innerHTML = `${formattedDateMonth} ${formattedDateYear}`;
+}
