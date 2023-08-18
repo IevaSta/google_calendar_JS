453ec750ff241ba4df047bec1e4b4326547f2c54
ievast <ievast@wix.com>
2023-08-18 14:59:04 +0300

    Storing day info in object, added style on today

--- js/components/renderSideCalendar.js
+++ js/components/renderSideCalendar.js
@@ -1,45 +1,52 @@
-export function renderSideCalendar(month, year) {
+export function renderSideCalendar(today, activeDay) {
   const sideCalendarListDOM = document.querySelector('.side-calendar__list');
   sideCalendarListDOM.innerHTML = '';
 
-  let day = 1;
+  const year = activeDay.getFullYear();
+  const month = activeDay.getMonth();
+
+  const firsWeekDayOfCrrMonth = new Date(year, month, 1).getDay(); //0-6 sunday-saturday
 
   //   https://stackoverflow.com/questions/222309/calculate-last-day-of-month
-  const daysInCrrMonth = new Date(year, month, 0).getDate(); //month ---> 0-11
-  const firsWeekDayOfCrrMonth = new Date(year, month - 1, 1).getDay(); //0-6 sunday-saturday
+  function getCalendarDays() {
+    const daysInPrevMonth = new Array(new Date(year, month - 1, 0).getDate()) //month ---> 0-11
+      .fill({})
+      .map((_, i) => {
+        return { number: i + 1, type: 'prev_month' };
+      })
+      .slice(-firsWeekDayOfCrrMonth);
 
-  const daysInPrevMonth = new Date(year, month - 1, 0).getDate(); //month ---> 0-11
-  //   [{ day: 1, type: 'current_month'}]
-  function addLiDOM(innerText, classList) {
+    const daysInCrrMonth = new Array(new Date(year, month, 0).getDate()) //month ---> 0-11
+      .fill({})
+      .map((_, i) => {
+        return { number: i + 1, type: 'crr_month' };
+      });
+
+    const daysInNextMonth = new Array(new Date(year, month + 1, 0).getDate()) //month ---> 0-11
+      .fill({})
+      .map((_, i) => {
+        return { number: i + 1, type: 'next_month' };
+      })
+      .slice(0, 42 - daysInPrevMonth.length - daysInCrrMonth.length);
+
+    return [...daysInPrevMonth, ...daysInCrrMonth, ...daysInNextMonth];
+  }
+
+  getCalendarDays(month, year).forEach((day) => {
     const sideCalendarDayDOM = document.createElement('li');
-    sideCalendarDayDOM.innerText = innerText;
+    sideCalendarDayDOM.innerText = day.number;
+
+    const classList =
+      day.type === 'crr_month'
+        ? ['side-calendar__day']
+        : ['side-calendar__day', 'not-curr-month'];
+
     sideCalendarDayDOM.classList.add(...classList);
+
+    if (Number(sideCalendarDayDOM.innerText) === today.getDate()) {
+      sideCalendarDayDOM.classList.add('crr-day');
+    }
+
     sideCalendarListDOM.appendChild(sideCalendarDayDOM);
-  }
-
-  //render days of before month
-  for (let x = firsWeekDayOfCrrMonth; x > 0; x--) {
-    const innerText = daysInPrevMonth - x + 1;
-    const classList = ['side-calendar__day', 'not-curr-month'];
-
-    addLiDOM(innerText, classList);
-  }
-
-  //render days of crr month
-  while (day <= daysInCrrMonth) {
-    const innerText = day++;
-    const classList = ['side-calendar__day'];
-
-    addLiDOM(innerText, classList);
-  }
-
-  //render days of next month
-  const daysInNextMonth = 42 - firsWeekDayOfCrrMonth - daysInCrrMonth;
-
-  for (let i = 1; i <= daysInNextMonth; i++) {
-    const innerText = i;
-    const classList = ['side-calendar__day', 'not-curr-month'];
-
-    addLiDOM(innerText, classList);
-  }
+  });
 }
