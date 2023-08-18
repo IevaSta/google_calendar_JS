3208424b52354662509c51df0cdaa720cd31458d
ievast <ievast@wix.com>
2023-08-18 15:01:52 +0300

    Created state handler, moved events to initinal fn

--- js/script.js
+++ js/script.js
@@ -1,8 +1,77 @@
-import { doTitleDate } from './components/doTitleDate.js';
+import { doTitleDate } from './components/renderSideTitle.js';
 import { renderModal } from './components/renderModal.js';
+import { renderSideCalendar } from './components/renderSideCalendar.js';
 
-const titleListDom = document.querySelectorAll('.render-title');
-
-titleListDom.forEach((title) => doTitleDate(title));
+const headertitleListDOM = document.querySelector('.render-title__side');
+const sideTitleDOM = document.querySelector('.side-calendar__title');
 
 renderModal();
+
+// ----STATE   state --> {today: , focusDay: }
+const initStateHandler = (initialState, render) => {
+  let state;
+
+  const stateHandler = {
+    setState(newState) {
+      state = newState;
+      render(stateHandler);
+    },
+    getState() {
+      return Object.freeze(state);
+    }
+  };
+
+  stateHandler.setState(initialState);
+  return stateHandler;
+};
+
+// ----INIT CALENDAR
+function startCalendar() {
+  const stateHandler = initStateHandler(
+    { today: new Date(), activeDay: new Date() },
+    render
+  );
+
+  const { setState, getState } = stateHandler;
+
+  function nextMonth() {
+    const newActiveDayDate = new Date(getState().activeDay);
+    newActiveDayDate.setMonth(newActiveDayDate.getMonth() + 1);
+
+    setState({
+      ...getState(),
+      activeDay: newActiveDayDate
+    });
+  }
+
+  function backMonth() {
+    const newActiveDayDate = new Date(getState().activeDay);
+    newActiveDayDate.setMonth(newActiveDayDate.getMonth() - 1);
+
+    setState({
+      ...getState(),
+      activeDay: newActiveDayDate
+    });
+  }
+
+  document
+    .querySelector('.forward-month__side')
+    .addEventListener('click', nextMonth);
+  document
+    .querySelector('.backward-month__side')
+    .addEventListener('click', backMonth);
+}
+
+// ----RENDERING
+function render(stateHandler) {
+  const { getState } = stateHandler;
+  const state = getState();
+
+  const todayData = state.today;
+  const activeDayData = state.activeDay;
+
+  doTitleDate(sideTitleDOM, activeDayData);
+  renderSideCalendar(todayData, activeDayData);
+}
+
+startCalendar();
