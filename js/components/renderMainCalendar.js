20b9e013be1af3ef61b6e87a95dcedf96323251e
ievast <ievast@wix.com>
2023-08-23 17:13:38 +0300

    Main calendars days, title, buttons logic

--- js/components/renderMainCalendar.js
+++ js/components/renderMainCalendar.js
@@ -1,18 +1,38 @@
 import { renderSideTitle } from './renderSideTitle.js';
 
-export function renderMainCalendar(today, activeDay) {
+export function renderMainCalendar(today, activeDay, clickedActiveDay) {
   const headertitleListDOM = document.querySelector('.render-title__header ');
   renderSideTitle(headertitleListDOM, activeDay);
 
-  const weekDayListDOM = document
-    .querySelectorAll('.week-day')
-    .forEach((weekDay) => (weekDay.innerHTML = ''));
+  const year = activeDay.getFullYear();
+  const month = activeDay.getMonth();
+  const day = activeDay.getDate();
 
-  const formatOptions = {
-    weekday: 'short'
-  };
+  const todayWeekDay = new Date(year, month, day).getDay(); //0-6 sunday-saturday
 
-  const test = new Intl.DateTimeFormat('en-US', formatOptions)
-    .format(activeDay)
-    .toUpperCase();
+  const currentWeekStart = new Date(activeDay);
+  currentWeekStart.setDate(day - todayWeekDay);
+
+  document.querySelectorAll('.month-day').forEach((dayDOM, i) => {
+    const renderingDay = new Date(currentWeekStart);
+    renderingDay.setDate(renderingDay.getDate() + i);
+
+    dayDOM.innerText = renderingDay.getDate();
+
+    dayDOM.addEventListener('click', () => clickedActiveDay(renderingDay));
+
+    dayDOM.classList.remove('crr-day');
+    dayDOM.classList.remove('active-day');
+
+    if (renderingDay.getDate() === today.getDate()) {
+      dayDOM.classList.add('crr-day');
+    }
+
+    if (
+      renderingDay.getDate() === activeDay.getDate() &&
+      activeDay.getDate() !== today.getDate()
+    ) {
+      dayDOM.classList.add('active-day');
+    }
+  });
 }
