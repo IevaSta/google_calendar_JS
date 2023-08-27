8d6123ded3be666138504b08e7abcc4dbcbd183e
ievast <ievast@wix.com>
2023-08-27 20:11:02 +0300

    add helper: time validation fn

---
+++ js/helpers/formTimeValidation.js
@@ -0,0 +1,38 @@
+import { formatTimeToHHMM } from './formatTimeToHHMM.js';
+
+export function formTimeValidation(date, start, end) {
+  let errorList = [];
+
+  const currentTime = new Date();
+  currentTime.setSeconds(0);
+  currentTime.setMilliseconds(0);
+
+  const choosenStartDate = new Date(date + ' ' + start);
+  const choosenEndDate = new Date(date + ' ' + end);
+
+  console.log(choosenStartDate.getTime() === currentTime.getTime());
+
+  errorList = [
+    ...errorList,
+    {
+      name: 'start',
+      msg:
+        choosenStartDate.getTime() < currentTime.getTime()
+          ? 'The event cannot start earlier than the present moment.'
+          : ''
+    }
+  ];
+
+  errorList = [
+    ...errorList,
+    {
+      name: 'end',
+      msg:
+        choosenStartDate.getTime() >= choosenEndDate.getTime()
+          ? 'The event cannot end earlier than the present moment.'
+          : ''
+    }
+  ];
+
+  return errorList;
+}
