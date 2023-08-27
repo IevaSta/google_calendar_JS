49bf496e4bce29447c08768b47f9f7d1d129b38b
ievast <ievast@wix.com>
2023-08-27 20:09:17 +0300

    Refactoring renderEvent fn

--- js/components/renderEvent.js
+++ js/components/renderEvent.js
@@ -1,41 +1,34 @@
-export function renderEvent() {
-  const title = document.querySelector('.event__form-header').value;
-  const timeStart = document.querySelector('.event-time__start').value;
-  const timeEnd = document.querySelector('.event-time__end').value;
-
+export function renderEvent(event) {
   //take weekday from date input
-  const timeDate = document.querySelector('.event-date').value;
-  const inputDate = new Date(timeDate);
+  const inputDate = new Date(event.date);
   const inputDateWeekDay = inputDate.getDay();
 
   //converting time to pixels
-  const eventTop = timeConverterToPixels(timeStart).toString();
+  const eventTop = timeConverterToPixels(event.start).toString();
   const eventLength = (
-    timeConverterToPixels(timeEnd) - timeConverterToPixels(timeStart)
+    timeConverterToPixels(event.end) - timeConverterToPixels(event.start)
   ).toString();
 
   const columnList = document.querySelectorAll('.lg-calendar__cell-list');
 
   //creating eventDom, adding style and content
-  if ((title, timeDate, timeStart, timeEnd)) {
-    const eventDOM = createDOM('div', 'event');
-    eventDOM.style.marginTop = `${eventTop}px`;
-    eventDOM.style.height = `${eventLength}px`;
+  const eventDOM = createDOM('div', 'event');
+  eventDOM.style.marginTop = `${eventTop}px`;
+  eventDOM.style.height = `${eventLength}px`;
 
-    const titleDOM = createDOM('span', 'event-title', title);
-    const timeStartDOM = createDOM('span', 'event-time', timeStart);
-    const timeEndDOM = createDOM('span', 'event-time', timeEnd);
+  const titleDOM = createDOM('span', 'event-title', event.title);
+  const timeStartDOM = createDOM('span', 'event-time', event.start);
+  const timeEndDOM = createDOM('span', 'event-time', event.end);
 
-    eventDOM.appendChild(titleDOM);
-    eventDOM.appendChild(timeStartDOM);
-    eventDOM.appendChild(timeEndDOM);
+  eventDOM.appendChild(titleDOM);
+  eventDOM.appendChild(timeStartDOM);
+  eventDOM.appendChild(timeEndDOM);
 
-    columnList.forEach((_, i) => {
-      if (i === inputDateWeekDay) {
-        columnList[i].prepend(eventDOM);
-      }
-    });
-  }
+  columnList.forEach((_, i) => {
+    if (i === inputDateWeekDay) {
+      columnList[i].prepend(eventDOM);
+    }
+  });
 }
 
 function createDOM(element, style, content) {
